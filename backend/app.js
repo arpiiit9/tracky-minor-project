54a0af8cc31f955c39db8d58a2c46fc4fbcb3366
Arpit Trivedi <89098148+arpiiit9@users.noreply.github.com>
2024-04-20 01:16:54 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT||5000
 
 //middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+ origin :"https://tracky-minor-project.vercel.app/"
+}))
 
 //routes
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
